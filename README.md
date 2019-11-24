595cdda6850ddbf6f1b4818214b272e4b6f83b5c
Denis Kalinin <denis@denis-macbook.local>
2019-11-24 13:37:48 +0300

    Tabbed window for iframes

--- README.md
+++ README.md
@@ -8,19 +8,24 @@ Demo: https://denis-kalinin.github.io/DialogForIFrame/
     <!-- Dialog -->
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/dialog-polyfill@0.5.0/dist/dialog-polyfill.css" />
     <link rel="stylesheet" type="text/css" href="https://denis-kalinin.github.io/DialogForIFrame/static/css/dialog.css" />
+    <!-- order is important: polyfill before dialog.js -->
     <script src="https://unpkg.com/dialog-polyfill@0.5.0/dist/dialog-polyfill.js"></script>
     <script src="https://denis-kalinin.github.io/DialogForIFrame/static/js/dialog.js"></script>
     ...
 </head>
 <body>
     ...
-    <dialog style="width:95%;height100%;padding:0;border:0">
-        <div class="xbutton" onclick="postMessage({dialog:null}, '*')"></div>
+    <dialog style="width:95%; height:90%; border:0; padding:0;">
+        <div class="tabbed">
+            <nav></nav>
+            <div class="tabwindow"></div>
+        </div>
     </dialog>
 </body>
 ```
+&ndash; set style to your dialog approprietely.
 
-From iframe send message to the parent window to open a new dialog:
+To open a new dialog send message from the iframe to the parent window (or `window.top`):
 ```javascript
-window.parent.postMessage('http://example.com', '*');
+window.parent.postMessage({dialog:{ur:'http://example.com'}}, '*');
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,12 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
 const path = require('path');
 //var lodash = require('lodash');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-module.exports = {
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [{ loader: "html-loader", options: { minimize: false } }]
-      },
-      {
-       test: /\.css$/,
-       use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  devtool: 'inline-source-map',
-  optimization: {
-    moduleIds: 'hashed',
-    minimize: false,
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
-    },
-    runtimeChunk: 'single',
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
-      title: "Webpacked",
-      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
-    }),
-    new CopyWebpackPlugin([
-      { from: 'src/static', to: 'static' },  
-    ]),
-  ],
-  output: {
-    filename: '[name].[contenthash].js',
-  },
-  externals : {
-    //lodash : '_'
-    //jquery: '$',
-    //datatables: 'databales.net-bs4'
-  },
-};
-//////PRODUCTION////////
-module.exports = {
-  mode: 'production',
+
+const config = {
+    /*
   module: {
     rules: [
       {
@@ -83,23 +33,44 @@ module.exports = {
     },
     runtimeChunk: 'single',
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebPackPlugin({
-      template: "src/index.html",
-      filename: "index.html",
-      title: "Webpacked",
-      meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
-    }),
-    new CopyWebpackPlugin([
-      { from: 'src/static', to: 'static' },      
-      //{ from: 'src/closeable.html'},     
-      //{ from: 'src/main.css'}
-    ]),
-  ],
+  */
+ plugins: [
+  new CopyWebpackPlugin([
+    { from: 'src/static', to: 'static' },
+  ]),
+],
   output: {
     path: path.resolve(__dirname, 'docs'),
-    filename: '[name].[contenthash].js',
+    //filename: '[name].[contenthash].js',
   },
-  externals : {},
+  externals : {
+    //lodash : '_'
+    //jquery: '$',
+  },
+}
+
+module.exports = ( env, argv ) => {
+  config.mode = argv.mode;
+  //////PRODUCTION////////
+  let theWebcontext;
+  if (config.mode === 'production') {
+    theWebcontext = '/DialogForIframe';
+    config.plugins.push(new CleanWebpackPlugin());
+  }
+  //////DEVELOPMENT////////
+  if (config.mode === 'development'){
+    theWebcontext = '';
+  }
+  const htmlPlugin = new HtmlWebPackPlugin({
+    template: "src/index.html",
+    filename: "index.html",
+    templateParameters: {
+      webcontext: theWebcontext
+    },
+    title: "Webpacked",
+    meta: {viewport: 'width=device-width, initial-scale=1, shrink-to-fit=no'}
+  });
+  config.plugins.push(htmlPlugin);
+
+  return config;
 };
