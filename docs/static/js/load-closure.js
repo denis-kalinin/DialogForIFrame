1a2493d163d32e4db04aa8857233a63f360a1062
Denis Kalinin <denis@itranga.com>
2020-03-02 18:41:57 +0300

    newclose.js with mulitple func

--- docs/static/js/load-closure.js
+++ docs/static/js/load-closure.js
@@ -1,1 +1,46 @@
-function close() { window.top.postMessage({dialog:null}, '*'); }(function(loadHandler){if(!loadHandler) throw new Error("\"loadHandler\" is null or undefined. If \"loadHandler\" is a function expression put it somewhere above.");if (!loadHandler instanceof Function) throw new Error('loadHandler is not a function.');var funcName = loadHandler.name;if(!funcName){var result = /^function\s+([\w\$]+)\s*\(/.exec( loadHandler.toString() );if(!result) throw new Error("Function expression in IE! DECLARE loadHandler: \"function onPageLoad(){...}\", DON'T express like \"var onPageLoad=function(){...}\"");funcName = result[1];}if(!window.opener){window[funcName] = function(){window.addEventListener('message', function(){loadHandler.apply(window);}, false);window.top.postMessage({dialog:{update:true}}, '*');}} else {loadHandler.apply(window);}})(onPageLoad);
+(function(w, loadHandlers){
+  function _isDialog(){
+    var dialogTag = 'DIALOG', el = w.frameElement || {};
+    while( (el=el.parentElement) && el.tagName.toUpperCase()!==dialogTag);
+    return !!el;
+  }
+  var isInDialog = !w.opener && _isDialog();
+  var openerUpdated = false;
+  for(var k=0; k<loadHandlers.length; k++){
+    var loadHandler = loadHandlers[k];
+    if(!loadHandler) throw new Error("\"loadHandler["+k+"]\" is null or undefined. If \"loadHandler\" is a function expression put it somewhere above.");
+    if (!loadHandler instanceof Function) throw new Error('loadHandler['+k+'] is not a function.');
+    var funcName = loadHandler.name;
+    if(!funcName){
+        var result = /^function\s+([\w\$]+)\s*\(/.exec( loadHandler.toString() );
+        if(!result) throw new Error("Function expression in IE! DECLARE loadHandler: \"function onPageLoad(){...}\", DON'T express like \"var onPageLoad=function(){...}\"");
+        funcName = result[1];
+    }
+    if(isInDialog){
+      (function(w, funcName, loadHandler){
+        w[funcName] = function(){
+          if(openerUpdated){
+            loadHandler.apply(w);
+          } else {
+            w.addEventListener('message', function(){
+              loadHandler.apply(w);
+            }, false);
+          }
+        };
+      })(w, funcName, loadHandler);
+    } else {
+      w[funcName] = function(){ loadHandler.apply(w); };
+    }
+  }
+  if(isInDialog){
+    var script = w.document.createElement('script');
+    script.textContent = "function close(){window.top.postMessage({dialog:null},'*');}";
+    w.document.head.appendChild(script);
+    (function(w){
+      w.addEventListener('message', function(){
+        openerUpdated = true;
+      }, false);
+    })(w);
+    w.top.postMessage({dialog:{update:true}}, '*');
+  }
+})(window.self, [testOnload, onPageLoad, testOnload2]);
